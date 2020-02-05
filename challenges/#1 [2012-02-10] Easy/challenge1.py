42b4d6724832e618f1df07a944a346e63efab265
Andres Suarez <andres27059934@gmail.com>
2020-02-05 20:19:24 -0300

    Solved Challenge 1

---
+++ challenges/#1 [2012-02-10] Easy/challenge1.py
@@ -0,0 +1,10 @@
+print('Enter your name:')
+name = input()
+
+print('Enter your age:')
+age = input()
+
+print('Enter your username:')
+username = input()
+
+print('Your name is '+name+', you are '+age+' years old, and your username is '+username+'.')
